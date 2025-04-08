6033f8f49dc6bdfb44b436179366e67cd68da187
nyk9 <eijikagimoto@gmail.com>
2025-04-08 14:25:08 +0900

    initial commit

---
+++ frontend/src/components/layouts/header/header.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "./mode-toggle";
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-2xl font-bold"></h1>
+      <ModeToggle />
+    </header>
+  );
+};
+export default Header;
