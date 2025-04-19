6e625da070e707ec784526fb56b4b1d763ca65d3
nyk9 <eijikagimoto@gmail.com>
2025-04-19 12:24:01 +0900

    feat: navigation menu to header

--- frontend/src/components/layouts/header/header.tsx
+++ frontend/src/components/layouts/header/header.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 const Header = () => {
   return (
-    <header>
-      <h1 className="text-2xl font-bold"></h1>
+    <header className="flex border">
+      <nav className="flex">
+        <Link href={"/"} className="p-1">
+          home
+        </Link>
+        <Link href={"/add"} className="p-1">
+          add
+        </Link>
+        <Link href={"/record"} className="p-1">
+          record
+        </Link>
+        <Link href={"/quiz"} className="p-1">
+          quiz
+        </Link>
+      </nav>
       <ModeToggle />
     </header>
   );
